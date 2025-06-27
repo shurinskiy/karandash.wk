119729846744760fdfbcfe9ec70223cc4ff61aa2
Alexandr Tumanov <shurinskiy2@gmail.com>
2025-06-27 21:20:29 +0500

    add footer

--- src/blocks/location/location.js
+++ src/blocks/location/location.js
@@ -5,12 +5,14 @@
 	const sibs = document.querySelectorAll('.location__items .location__marker');
 
 	markers.forEach(marker => {
-		marker.addEventListener('click', e => {
-			const matchItem = [...sibs].find(sib => sib.dataset.id == marker.dataset.id)?.parentNode;
-
-			[...markers, ...items].forEach(el => el.classList.remove('active'));
-			matchItem?.classList.add('active');
-			marker.classList.add('active');			
+		['click', 'mouseenter'].forEach(event => {
+			marker.addEventListener(event, e => {
+				const matchItem = [...sibs].find(sib => sib.dataset.id == marker.dataset.id)?.parentNode;
+	
+				[...markers, ...items].forEach(el => el.classList.remove('active'));
+				matchItem?.classList.add('active');
+				marker.classList.add('active');			
+			});
 		});
 	});
 
