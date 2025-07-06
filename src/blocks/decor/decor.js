f96e2564221c1d5590ba5b4f4302bb82fc70ecff
Alexandr Tumanov <shurinskiy2@gmail.com>
2025-07-06 22:20:58 +0500

    start animation work

--- src/blocks/decor/decor.js
+++ src/blocks/decor/decor.js
@@ -1,4 +1,33 @@
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
 (() => {
 	
+	gsap.registerPlugin(ScrollTrigger);
+
+	gsap.matchMedia().add("(min-width: 1101px)", () => {
+		document.querySelectorAll('.decor').forEach(el => {
+			const distance = parseFloat(el.dataset.gsapDist) || 500;
+			const direction = el.dataset.gsapDir ?? 'y';
+
+			const from = {
+				...(direction === 'x' ? { x: distance } : { y: distance }),
+			};
+
+			const to = {
+				x: direction === 'x' ? 0 : undefined,
+				y: direction === 'y' ? 0 : undefined,
+				ease: 'none',
+				scrollTrigger: {
+					trigger: el,
+					start: 'top bottom',
+					end: 'bottom top',
+					scrub: true,
+				},
+			};
+
+			gsap.fromTo(el, from, to);
+		});
+	});
 
 })();
