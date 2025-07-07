5e08254cdb28f8d6a4455389763cf1e3e97955a4
Alexandr Tumanov <shurinskiy2@gmail.com>
2025-07-07 22:54:06 +0500

    fix decors

--- src/blocks/decor/decor.js
+++ src/blocks/decor/decor.js
@@ -2,32 +2,45 @@ import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 (() => {
-	
 	gsap.registerPlugin(ScrollTrigger);
 
-	gsap.matchMedia().add("(min-width: 1101px)", () => {
+	gsap.matchMedia().add('(min-width: 1101px)', () => {
 		document.querySelectorAll('.decor').forEach(el => {
-			const distance = parseFloat(el.dataset.gsapDist) || 500;
-			const direction = el.dataset.gsapDir ?? 'y';
+			const dir = el.dataset.gsapDir ?? 'y';
+			const dist = parseFloat(el.dataset.gsapDist) || 500;
+			const { top, bottom } = el.getBoundingClientRect();
+			const inView = top < window.innerHeight && bottom > 0;
 
-			const from = {
-				...(direction === 'x' ? { x: distance } : { y: distance }),
-			};
-
-			const to = {
-				x: direction === 'x' ? 0 : undefined,
-				y: direction === 'y' ? 0 : undefined,
+			gsap.to(el, {
+				[dir]: -dist,
 				ease: 'none',
 				scrollTrigger: {
 					trigger: el,
-					start: 'top bottom',
+					start: inView ? () => `top ${el.getBoundingClientRect().top}px` : 'top bottom',
 					end: 'bottom top',
 					scrub: true,
-				},
-			};
-
-			gsap.fromTo(el, from, to);
+				}
+			});
 		});
 	});
+
+	/* document.querySelectorAll('.hero__decor').forEach(el => {
+		const start = el.getBoundingClientRect().top;
+		const distance = parseFloat(el.dataset.gsapDist) || 500;
+		const direction = el.dataset.gsapDir ?? 'y';
+
+		gsap.to(el, {
+				[direction]: -distance,
+				ease: "none",
+				scrollTrigger: {
+					trigger: el,
+					start: () => `top ${start}px`,
+					end: 'bottom top',
+					scrub: true,
+					markers: true
+				}
+			}
+		);
+	}); */
 
 })();
