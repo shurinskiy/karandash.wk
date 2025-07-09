7bde57edaeb5f88469710a4ced631a56284ce02a
Alexandr Tumanov <shurinskiy2@gmail.com>
2025-07-09 16:11:30 +0500

    fix hero layout

--- src/blocks/decor/decor.js
+++ src/blocks/decor/decor.js
@@ -6,42 +6,25 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger';
 	gsap.registerPlugin(ScrollTrigger);
 
 	gsap.matchMedia().add('(min-width: 1101px)', () => {
-		document.querySelectorAll('.decor').forEach(el => {
+		const { top: bodyTop } = document.body.getBoundingClientRect();
+
+		document.querySelectorAll('.decor').forEach((el, i) => {
 			const dir = el.dataset.gsapDir ?? 'y';
-			const dist = parseFloat(el.dataset.gsapDist) || 500;
-			const { top, bottom } = el.getBoundingClientRect();
-			const inView = top < window.innerHeight && bottom > 0;
+			const dist = +el.dataset.gsapDist || 500;
+			const { top } = el.getBoundingClientRect();
+			const inHero = top - bodyTop < window.innerHeight;
 
 			gsap.to(el, {
 				[dir]: -dist,
 				ease: 'none',
 				scrollTrigger: {
 					trigger: el,
-					start: inView ? () => `top ${el.getBoundingClientRect().top}px` : 'top bottom',
+					start: inHero ? () => `top ${top - bodyTop}px` : 'top bottom',
 					end: 'bottom top',
 					scrub: true,
 				}
 			});
 		});
 	});
-
-	/* gsap.matchMedia().add('(min-width: 1101px)', () => {
-		document.querySelectorAll('.decor').forEach(el => {
-			const dir = el.dataset.gsapDir ?? 'y';
-			const dist = parseFloat(el.dataset.gsapDist) || 500;
-			const inHero = el.offsetTop >= window.scrollY && el.offsetTop <= window.scrollY + window.innerHeight;
-
-			gsap.to(el, {
-				[dir]: -dist,
-				ease: 'none',
-				scrollTrigger: {
-					trigger: el,
-					start: inHero ? () => `top ${el.getBoundingClientRect().top}px` : 'top bottom',
-					end: 'bottom top',
-					scrub: true,
-				}
-			});
-		});
-	}); */
 
 })();
